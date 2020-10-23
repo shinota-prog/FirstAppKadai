0eeea4e80fe9634e4b0534750958764129b5381d
shin.ota <shin.ota0817@gmail.com>
2020-10-23 09:57:09 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Shin Ota on 10/23/20.
+//  Copyright © 2020 Shin Ota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
